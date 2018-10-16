eb687f281781aece05fcd349eff9007d37518c02
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-10-16 18:33:55 +0700

    initial and jump straight into the tutorial codes

---
+++ app/src/main/java/com/github/hellokotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textHello = findViewById<TextView>(R.id.text_hello)
+        textHello.text = "Hello Kotlin!" // actually, i want to use getString but, nyeh... let's follow the tutorial first :D
+    }
+}
